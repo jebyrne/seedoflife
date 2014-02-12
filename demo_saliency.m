6291a5327bb03efeeced94dd6bd6c376ad42bddc
Jeffrey Byrne <jebyrne@gmail.com>
2014-02-12 11:31:19 -0500

    saliency updates

--- demo_saliency.m
+++ demo_saliency.m
@@ -1,4 +1,4 @@
-function [] = demo_saliency(imfile)
+function [imsaliency, imgrey, imblend] = demo_saliency(imfile)
 %--------------------------------------------------------------------------
 %
 % Copyright (c) 2014 Jeffrey Byrne 
@@ -13,4 +13,8 @@ imgrey = nsd.util.im2gray(imfile);
 imsaliency = nsd.saliency(imfile);
 imsaliency_rgb = ind2rgb(round(255*mat2gray(imsaliency)), jet(256));
 imgrey_rgb = ind2rgb(round(255*imgrey), gray(256));
-figure(1); clf; imagesc(0.25*imgrey_rgb + 0.75*imsaliency_rgb); axis equal; axis off;
+imblend = 0.25*imgrey_rgb + 0.75*imsaliency_rgb;
+
+if nargout == 0
+  figure(1); clf; imagesc(imblend); axis equal; axis off;
+end
